15357543ac325d6545d050136642c5cfc4cfc69d
Raikoen <cole.kairu@gmail.com>
2022-12-17 12:14:47 -0600

    Added My Progress to the navigation stack

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ const Stack = createNativeStackNavigator();
 
 import HomeScreen from './screens/home.js';
 import LessonScreen from './screens/lessons.js';
+import MyProgress from './screens/myProgress.js';
 import Mod1Screen from './screens/module1.js';
 import Mod2Screen from './screens/module2.js';
 import Mod3Screen from './screens/module3.js';
+
 
 class App extends React.Component {
   render() {
@@ -23,6 +25,7 @@ class App extends React.Component {
       <Stack.Navigator initialRouteName={HomeScreen}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Lessons" component={LessonScreen} />
+        <Stack.Screen name="My Progress" component={MyProgress}/>
         <Stack.Screen name="Lesson 1" component={Mod1Screen}/>
         <Stack.Screen name="Lesson 2" component={Mod2Screen}/>
         <Stack.Screen name="Lesson 3" component={Mod3Screen}/>
